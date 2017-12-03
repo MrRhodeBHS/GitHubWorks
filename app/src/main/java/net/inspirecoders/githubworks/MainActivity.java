22d9ace85b74f4dd21c3e9716188717b6c740a13
MrRhodeBHS <drhode@barabooschools.net>
2017-12-02 19:30:33 -0600

    Initial Commit

---
+++ app/src/main/java/net/inspirecoders/githubworks/MainActivity.java
@@ -0,0 +1,21 @@
+package net.inspirecoders.githubworks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void buttonNext(View v)
+    {
+        Log.i("info", "Next Clicked");
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
